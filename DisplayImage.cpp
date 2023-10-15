6ed4b67de80005dd8c42bc64105bde63bd08f63a
Arthur Neves <arthurneves@pop-os.localdomain>
2023-10-15 17:04:12 -0300

    interpolaçãoFuncionando

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -5,30 +5,11 @@
 #include<vector>
 
 
-/*
-        openCV confia que colocaremos o tipo certo
-        Nomes dos tipos seguem esse padrao:
-CV_<bit_count><itentifier><num_of_channels>
-    exemplo:
-RGB image 8 bit-unsined is CV_8UC3
-gray scale 8 bit-unsined is CV_8UC1
-better to use DataType
-example         - DataType<uint>::type =CV8UC1
-toda imagem é um cv::Mat -- Matriz
-
-
-
-*/
-
-void dobrarImagem(unsigned char * imagemNormal,unsigned char * imagemAumentada){
+void dobrarImagem(unsigned char * imagemNormal,unsigned char * imagemAumentada,int quantColunaMatrizMenor = 176,int quantLinhaMatrizMenor =144, int quantColunaMatrizMaior =352 ,int quantLinhaMatrizMaior = 288){
     unsigned char *ptrImagemNormal = imagemNormal;
     unsigned char *ptrImagemAumentada = imagemAumentada;
-    int quantColunaMatrizMaior =352;
-    int quantLinhaMatrizMaior = 288;
-    int a =0;
-    //colocando zeros nas linhas e colunas
-    for(unsigned int i =0;i<quantLinhaMatrizMaior/2;i++){//linhas menor
-        for(unsigned int j=0;j<quantColunaMatrizMaior/2;j++){//colunas Menor
+    for(unsigned int i =0;i<quantLinhaMatrizMenor;i++){//linhas menor
+        for(unsigned int j=0;j<quantColunaMatrizMenor;j++){//colunas Menor
             *ptrImagemAumentada= *ptrImagemNormal;
             ptrImagemAumentada++;
             *ptrImagemAumentada=0;
@@ -41,29 +22,56 @@ void dobrarImagem(unsigned char * imagemNormal,unsigned char * imagemAumentada){
         }
         
     }
+}
 
-    ptrImagemAumentada=imagemAumentada;
+void interpolacaoPadrao(unsigned char * imagemParaInterpolar,int quantColunaMatrizMaior =352 ,int quantLinhaMatrizMaior = 288){
+    unsigned char *ptrImagemInterpolar = imagemParaInterpolar;
     unsigned char superior;
     unsigned char esquerdo;
     for(int linhaAtual =0;linhaAtual<quantLinhaMatrizMaior;linhaAtual++){
         for( int colunaAtual =0;colunaAtual<quantColunaMatrizMaior;colunaAtual++){
             if(linhaAtual%2==0){
                 if(colunaAtual%2!=0){
-                    esquerdo = ptrImagemAumentada[linhaAtual*quantColunaMatrizMaior+colunaAtual -1];
-                    ptrImagemAumentada[linhaAtual*quantColunaMatrizMaior+colunaAtual]=esquerdo;
+                    esquerdo = ptrImagemInterpolar[linhaAtual*quantColunaMatrizMaior+colunaAtual -1];
+                    ptrImagemInterpolar[linhaAtual*quantColunaMatrizMaior+colunaAtual]=esquerdo;
                 }
             }
             else{
-                superior = ptrImagemAumentada[linhaAtual*quantColunaMatrizMaior+colunaAtual-quantColunaMatrizMaior];
-                ptrImagemAumentada[linhaAtual*quantColunaMatrizMaior+colunaAtual]=superior;
+                superior = ptrImagemInterpolar[linhaAtual*quantColunaMatrizMaior+colunaAtual-quantColunaMatrizMaior];
+                ptrImagemInterpolar[linhaAtual*quantColunaMatrizMaior+colunaAtual]=superior;
             }
         }
     }
-
 }
 
+void interPololacaoMedia(unsigned char * imagemParaInterpolar,int quantColunaMatrizMaior =352 ,int quantLinhaMatrizMaior = 288){
+    unsigned char *ptrImagemInterpolar = imagemParaInterpolar;
+    unsigned char superior;
+    unsigned char media;
+    unsigned char esquerdo;
+    for(int linhaAtual =0;linhaAtual<quantLinhaMatrizMaior;linhaAtual++){
+        for( int colunaAtual =0;colunaAtual<quantColunaMatrizMaior;colunaAtual++){
+            if(linhaAtual%2==0){
+                if(colunaAtual%2!=0&&colunaAtual<quantColunaMatrizMaior-1){
+                    media = (ptrImagemInterpolar[linhaAtual*quantColunaMatrizMaior+colunaAtual -1]+ptrImagemInterpolar[linhaAtual*quantColunaMatrizMaior+colunaAtual +1])/2;
+                    ptrImagemInterpolar[linhaAtual*quantColunaMatrizMaior+colunaAtual]=media;
+                }
+                else if(colunaAtual%2!=0&&colunaAtual==quantColunaMatrizMaior-1){
+                    esquerdo = ptrImagemInterpolar[linhaAtual*quantColunaMatrizMaior+colunaAtual -1];
+                    ptrImagemInterpolar[linhaAtual*quantColunaMatrizMaior+colunaAtual]=esquerdo;
+                }
+                
+            }
+            else{
+                media = ptrImagemInterpolar[linhaAtual-1*quantColunaMatrizMaior +colunaAtual];
+                superior = ptrImagemInterpolar[linhaAtual*quantColunaMatrizMaior+colunaAtual-quantColunaMatrizMaior];
+                ptrImagemInterpolar[linhaAtual*quantColunaMatrizMaior+colunaAtual]=superior;
+            }
+        }
+    }
+}
 
-void lerYuV(std::string nome, int linhas,int colunas,int frame=0){
+void lerYuV(std::string nome, int colunas,int linhas,int frame=0){
     frame--;
     //1.1
     std::ifstream fin;//cria o objeto para leitura do arquivo.
@@ -85,23 +93,28 @@ void lerYuV(std::string nome, int linhas,int colunas,int frame=0){
     unsigned char uMaior[colunas][linhas];
     unsigned char vMaior[colunas][linhas];
 
-    cv::Mat imagemJunta;
-    cv::Mat imagemColorida=cv::Mat(colunas,linhas,CV_8UC3) ;
 
-    cv::Mat imageAntes = cv::Mat(colunas/2,linhas/2,CV_8U,v);
-    cv::namedWindow("original",cv::WINDOW_AUTOSIZE);
-    cv::imshow("original",cv::Mat(colunas/2,linhas/2,CV_8U, v));
-    dobrarImagem((unsigned char *)v,*vMaior);
-    dobrarImagem((unsigned char *)u,*uMaior);
-    cv::Mat image= cv::Mat(colunas,linhas,CV_8U,vMaior);
-
+    cv::Mat imageAntes = cv::Mat(linhas/2,colunas/2,CV_8U,v);
+    cv::namedWindow("original",cv::WINDOW_FULLSCREEN|cv::WINDOW_AUTOSIZE);
+    //cv::imshow("original",cv::Mat(linhas/2,colunas/2,CV_8U, v));
+    
+    dobrarImagem((unsigned char *)v,*vMaior,colunas/2,linhas/2,colunas,linhas);
+    interPololacaoMedia(*vMaior,colunas,linhas);
+    dobrarImagem((unsigned char *)u,*uMaior,colunas/2,linhas/2,colunas,linhas);
+    interPololacaoMedia(*uMaior,colunas,linhas);
+    
     std::vector<cv::Mat> imagemReconstruída;
-    imagemReconstruída.push_back(cv::Mat(colunas,linhas,CV_8U,y));
-    imagemReconstruída.push_back(cv::Mat(colunas,linhas,CV_8U,uMaior));
-    imagemReconstruída.push_back(cv::Mat(colunas,linhas,CV_8U,vMaior));
+    imagemReconstruída.push_back(cv::Mat(linhas,colunas,CV_8U,y));
+    imagemReconstruída.push_back(cv::Mat(linhas,colunas,CV_8U,uMaior));
+    imagemReconstruída.push_back(cv::Mat(linhas,colunas,CV_8U,vMaior));
+
+    cv::Mat imagemJunta;
+    cv::Mat imagemColorida=cv::Mat(linhas,colunas,CV_8UC3) ;
+
+
     cv::merge(imagemReconstruída,imagemJunta);
     cv::cvtColor(imagemJunta,imagemColorida,cv::COLOR_YUV2BGR);
-    cv::imshow("COLORIDAAA",imagemColorida);
+    cv::imshow("original",imagemColorida);
     cv::waitKey(0);
 }
 
