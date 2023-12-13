a45d05551ce0b40928c617481ce9def314b1777e
Arthur Neves <arthurneves@pop-os.localdomain>
2023-12-13 19:56:45 -0300

    trabalho terminado

--- Projeto2/Projeto2.cpp
+++ Projeto2/Projeto2.cpp
@@ -7,6 +7,8 @@
 
 void imgHistograma(cv::Mat src);
 void identificacaoTumor(cv::Mat imagem);
+void primeiraQuestao();
+void segundaQuestao();
 void imgHistograma(cv::Mat src){
     cv::Mat histograma;
     //eu crio uma Mat de zeros pois eu irei ficar incrementando o valor dos pixels, ou seja, quanto mais
@@ -80,8 +82,8 @@ void identificacaoTumor(cv::Mat imagem){
         }
     }    
     cv::Mat MascaraTumor = labelImage==largestLabel;
-    cv::imshow("threhold",imgTreshHold);
-     std::vector<std::vector<cv::Point> > contours;
+    // cv::imshow("threhold",imgTreshHold);
+    std::vector<std::vector<cv::Point> > contours;
     cv::findContours(MascaraTumor,contours,cv::RETR_CCOMP,cv::CHAIN_APPROX_SIMPLE);
     cv::cvtColor(imagem,imagem,cv::COLOR_GRAY2RGB);
     cv::drawContours(imagem,contours,-1,cv::Scalar(0,255,0));
@@ -91,18 +93,83 @@ void identificacaoTumor(cv::Mat imagem){
     
 }
 
-int main(){
-    cv::Mat img = cv::imread("imagesT2png/brain.png",cv::IMREAD_GRAYSCALE);
-    // cv::imshow("Imagem Original", img);
+void primeiraQuestao(){
+    cv::Mat imgBrain = cv::imread("imagesT2png/brain.png",cv::IMREAD_GRAYSCALE);
+    // cv::imshow("Imagem Original", imgBrain);
     // cv::waitKey(0);
-    cv::Mat imgFiltered;    
-    cv::GaussianBlur(img, imgFiltered, cv::Size(5,5), 0, 0);
-    // cv::imshow("Gaussian Blur", img);
-    cv::medianBlur(img,imgFiltered,3);
-    // cv::imshow("Median Blur", img);
-    //A partir daqui vou aplicar o histograma na img
-    imgHistograma(imgFiltered);
-    identificacaoTumor(imgFiltered);
+    cv::Mat imgBrainFiltered;    
+    cv::GaussianBlur(imgBrain, imgBrainFiltered, cv::Size(5,5), 0, 0);
+    // cv::imshow("Gaussian Blur", imgBrain);
+    cv::medianBlur(imgBrain,imgBrainFiltered,3);
+    // cv::imshow("Median Blur", imgBrain);
+    //A partir daqui vou aplicar o histograma na imgBrain
+    imgHistograma(imgBrainFiltered);
+    identificacaoTumor(imgBrainFiltered);
+}
+void segmentacao(cv::Mat img){
+    
+}
+
+
+cv::Mat imagemQuantizacao(cv::Mat img, int numeroDeClusters, int iteracoes){
+//primeiro mapeamos a nossa fonte para amostras
+//uma matriz de 26730 linhas com 3 colunas(quantida de canais da img de input) e 1 canal
+cv::Mat amostras(img.rows * img.cols, img.channels(), CV_32F);
+// std::cout<<samples.cols<<" "<<samples.rows<<" "<<samples.channels();
+    //vamos agora iterar a nossa img de entrada para preencher a amostra;
+    for (int y = 0; y < img.rows; y++){
+        for (int x = 0; x < img.cols; x++){
+            for (int channel = 0; channel < img.channels(); channel++){
+                if(img.channels()==3){
+                    amostras.at<float>(y+x*img.rows,channel)=img.at<cv::Vec3b>(y,x)[channel];
+                }
+                else{
+                    amostras.at<float>(y+x*img.rows,channel)=img.at<float>(y,x);
+                }    
+            }
+        }
+    }
+    cv::Mat labels;
+    int attempts = 5;
+    cv::Mat centers;
+    cv::kmeans(amostras,numeroDeClusters,labels,
+    cv::TermCriteria(cv::TermCriteria::EPS|cv::TermCriteria::MAX_ITER,iteracoes,0.0001),attempts,cv::KMEANS_PP_CENTERS,centers);
+    cv::Mat imgClusterizada(img.size(),img.type());
+    for (int y = 0; y < img.rows; y++)
+		for (int x = 0; x < img.cols; x++)
+		{
+			int cluster_idx = labels.at<int>(y + x * img.rows, 0);
+			if (img.channels()==3) {
+				for (int i = 0; i < img.channels(); i++) {
+					imgClusterizada.at<cv::Vec3b>(y, x)[i] = centers.at<float>(cluster_idx, i);
+				}
+			}
+			else {
+				imgClusterizada.at<unsigned char>(y, x) = centers.at<float>(cluster_idx, 0);
+			}
+		}
+	imshow("clustered image 6", imgClusterizada);
+    cv::waitKey(0);
+	return imgClusterizada;
+
+    
+}
+
+void segundaQuestao(){
+    cv::Mat imgOrion = cv::imread("/home/arthurneves/Projetos/IPI/Projeto2/imagesT2png/onion.png");
+    // cv::imshow("orion",imgOrion);
+    // cv::waitKey(0);
+    int numeroDeClusters = 10;
+    int iteracoes = 100;
+    cv::Mat imagemSegmentada = imagemQuantizacao(imgOrion,6,iteracoes);
+    // cv::imshow("img clusterizada", labels);
+    // cv::waitKey(0);
+    
+
+}
+int main(){
+    // primeiraQuestao();
+    segundaQuestao();
 
 
     return 0;
